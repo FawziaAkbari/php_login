26a20a8a4b08dba7f04355e7d4f2435e01ca11cb
FawziaAkbari <57459554+FawziaAkbari@users.noreply.github.com>
2022-10-25 07:47:39 +0430

    add delete button

---
+++ index.js
@@ -0,0 +1,7 @@
+// const btn = document.getElementById("btn")
+// const img = document.getElementById("img")
+
+
+// btn.addEventListener('click', function() {
+//     img.style.display = 'none'
+// })
--- welcome.php
+++ welcome.php
@@ -1,26 +1,51 @@
 </<?php 
     session_start();
+
+    if(!isset($_SESSION['usernInfo'])) {
+        header("location:index.php?try=again");
+    }
 ?>
 
 <html>
     <link rel="stylesheet" href="style.css">
     <body>
-    <?php
-
-        if(!isset($_SESSION['usernInfo'])) {
-            header("location:index.php?try=again");
-        }
-  
-    ?>  
         <div class="article">
             <h2 class="welcome-msg">Welcome To Our Website dear <span class="information"><?php echo $_SESSION['usernInfo']['name']?> </span></h2>
             <h4>Your name is: <span class="information"><?php echo $_SESSION['usernInfo']['name']?> </span></h4>
             <h4>Your age: <span class="information"><?php echo $_SESSION['usernInfo']['age']?> </span></h4>
             <h4>Youe email: <span class="information"><?php echo $_SESSION['usernInfo']['email']?> </span></h4>
             <h4>Your password: <span class="information"><?php echo $_SESSION['usernInfo']['password']?> </span></h4>
-            <img src="images/<?php echo $_SESSION['usernInfo']['photo']?> ?>" alt="your profile" class="image-user">
-            <div class="btn">
-                <a href="session.php" >Log me out</a>
+            <div id="img">
+                    <img src="images/<?php echo $_SESSION['usernInfo']['photo'];  ?>" alt=""  class="image-user">
+            </div>
+            
+            <?php
+                	if( isset( $_POST['submit'] )){
+                        $directory = 'images/'. $_SESSION['usernInfo']['photo'];
+                        $exists = file_exists($directory);
+                        
+                        if($exists){
+                            unlink($directory);
+                        
+                        }else{
+                      
+                            echo "The photo was deleted";
+                        }
+                    }
+            ?>
+           
+              
+          
+            <!-- button delete -->
+            <div>
+                <form action="" method="POST">
+                        <button id="btn-delete" class="btn-delete" type="submit" name="submit">Delete Photo</button>
+                </form>
+                <!-- link -->
+                <div class="btn">
+                    <a href="session.php" >Log me out</a>
+                </div>
+
             </div>
         </div>
     </body>
